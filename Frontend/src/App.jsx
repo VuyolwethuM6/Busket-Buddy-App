b50157e4721ddf169eac9179cf4abb7d392b9b9b
VuyolwethuM6 <vmabhuleka@gmail.com>
2024-03-06 14:36:19 +0200

    Rended the login page.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,65 @@
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Login from './components/Login/login'
 import Splash from './components/Splash/Splash'
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLogin(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <>
-      <Splash/>
-    </>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          {!showLogin && <Route path="/" element={<Splash />} />}
+          {showLogin && <Route path="/" element={<Login />} />}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+
+// import { useState, useEffect } from 'react';
+// import { Route, Routes, Navigate } from 'react-router-dom';
+// import './App.css'
+// import Login from './components/Login/login'
+// import Splash from './components/Splash/Splash'
+
+// function App() {
+
+//   const [showLogin, setShowLogin] = useState(false);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setShowLogin(true);
+//     }, 3000);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <Routes>
+//         {!showLogin && <Route path="/" element={<Splash />} />}
+//         {showLogin && <Route path="/" element={<Navigate to="/login" />} />}
+//         <Route path="/login" element={<Login />} />
+        
+//       </Routes>
+//     </div>
+//   )
+// }
+// export default App
