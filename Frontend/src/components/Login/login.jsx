9a25365e3836ed9fba6e9c3c70715c03c7f23bce
VuyolwethuM6 <vmabhuleka@gmail.com>
2024-03-11 11:37:45 +0200

    Connected the components with the backend

--- Frontend/src/components/Login/login.jsx
+++ Frontend/src/components/Login/login.jsx
@@ -1,39 +1,119 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import './login.css';
-import google_icon from '../../assets/icons/google.svg';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import google_icon from '../../assets/icons/google.svg'
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = inputValue;
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const handleError = (err) => toast.error(err, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });
+
+
+  const handleSuccess = (msg) => toast.success(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });
+  // toast.success(msg, {position: "bottom-left", })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/login",
+        {
+          ...inputValue,
+        },
+        { withCredentials: true }
+      );
+
+      console.log(data);
+      // window.alert(data);
+
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/dashboard");
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setInputValue({
+      ...inputValue,
+      email: "",
+      password: "",
+    });
+  };
+
+
   return (
     <div className="page_container">
       <div className="card">
         <div className="form_container">
           <h2>Login Account</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="input_container">
               <input
                 type="email"
                 name="email"
-                placeholder=" "
+                value={email}
+                onChange={handleOnChange}
               />
-              <label htmlFor="email">Email</label>
+            <label htmlFor="email">Email</label>
             </div>
             <div className="input_container">
               <input
                 type="password"
                 name="password"
-                placeholder=" "
+                value={password}
+                onChange={handleOnChange}
               />
-              <label htmlFor="password">Password</label>
+             <label htmlFor="password">Password</label>
             </div>
             <button type="submit">Submit</button>
 
-            <p className="forgot_password">
+            <span className="forgot_password">
               Forgot password?
-            </p>
+            </span>
 
           </form>
           <div className="google_login_container">
-            <img src={google_icon} alt="Google icon" style={{ width: '110px', height:'50px'}}/>
-            <span> Login with Google</span>
+              <img src={google_icon} alt="Google icon" style={{ width: '110px', height:'50px'}}/>
+              <span> Login with Google</span>
           </div>
 
           <div className="dont_have_account">
@@ -41,8 +121,63 @@ const Login = () => {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
 
 export default Login;
+
+
+
+
+
+
+// import './login.css';
+// import google_icon from '../../assets/icons/google.svg';
+
+// const Login = () => {
+//   return (
+//     <div className="page_container">
+//       <div className="card">
+//         <div className="form_container">
+//           <h2>Login Account</h2>
+//           <form>
+//             <div className="input_container">
+//               <input
+//                 type="email"
+//                 name="email"
+//                 placeholder=" "
+//               />
+//               <label htmlFor="email">Email</label>
+//             </div>
+//             <div className="input_container">
+//               <input
+//                 type="password"
+//                 name="password"
+//                 placeholder=" "
+//               />
+//               <label htmlFor="password">Password</label>
+//             </div>
+//             <button type="submit">Submit</button>
+
+//             <p className="forgot_password">
+//               Forgot password?
+//             </p>
+
+//           </form>
+//           <div className="google_login_container">
+//             <img src={google_icon} alt="Google icon" style={{ width: '110px', height:'50px'}}/>
+//             <span> Login with Google</span>
+//           </div>
+
+//           <div className="dont_have_account">
+//             Don&apos;t have an account? <a href="http://localhost:5173/signup" className="signup_link">Sign up</a>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
--- Frontend/src/components/Signup/signup.jsx
+++ Frontend/src/components/Signup/signup.jsx
@@ -1,19 +1,95 @@
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import './signup.css';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Signup = () => {
+
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const { email, password, username } = inputValue;
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const handleError = (err) => toast.error(err, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });
+
+
+  // const handleSuccess = (msg) => window.alert(msg);
+  const handleSuccess = (msg) => toast.success(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+    });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/signup",
+        {
+          ...inputValue,
+        },
+        { withCredentials: true }
+      );
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setInputValue({
+      ...inputValue,
+      email: "",
+      password: "",
+      username: "",
+    });
+  };
+
   return (
-    <div className='sign_up'>z
+    <div className='sign_up'>
       <div className="signup_container">
         <div className="card">
-          <h2 >Sign Up Account</h2>
-          <form>
+          <h2 className="signup_heading">Sign Up Account</h2>
+          <form onSubmit={handleSubmit}>
             <div className="input_container">
               <input
                 type="text"
-                name="name"
+                name="username"
                 placeholder=" "
                 className="input_field"
+                value={username}
+                onChange={handleOnChange}
               />
               <label htmlFor="name" className="floating_label">Name</label>
               {/* Name validation code here */}
@@ -24,6 +100,8 @@ const Signup = () => {
                 name="email"
                 placeholder=" "
                 className="input_field"
+                value={email}
+                onChange={handleOnChange}
               />
               <label htmlFor="email" className="floating_label">Email</label>
               {/* Email validation code here */}
@@ -34,20 +112,78 @@ const Signup = () => {
                 name="password"
                 placeholder=" "
                 className="input_field"
+                value={password}
+                onChange={handleOnChange}
               />
               <label htmlFor="password" className="floating_label">Password</label>
               {/* Password validation code here */}
             </div>
             <div className="login_link">
-              Already have an account? &nbsp;&nbsp;<a href="http://localhost:5173/login">Login </a>
+              Already have an account? &nbsp;&nbsp;<a href="http://localhost:5174/login">Login </a>
             </div>
             <button type="submit" className="submit_button">Submit</button>
-            
           </form>
         </div>
+        <ToastContainer />
       </div>
     </div>
   );
 };
 
 export default Signup;
+
+
+
+
+// import './signup.css';
+
+// const Signup = () => {
+//   return (
+//     <div className='sign_up'>z
+//       <div className="signup_container">
+//         <div className="card">
+//           <h2 >Sign Up Account</h2>
+//           <form>
+//             <div className="input_container">
+//               <input
+//                 type="text"
+//                 name="name"
+//                 placeholder=" "
+//                 className="input_field"
+//               />
+//               <label htmlFor="name" className="floating_label">Name</label>
+//               {/* Name validation code here */}
+//             </div>
+//             <div className="input_container">
+//               <input
+//                 type="email"
+//                 name="email"
+//                 placeholder=" "
+//                 className="input_field"
+//               />
+//               <label htmlFor="email" className="floating_label">Email</label>
+//               {/* Email validation code here */}
+//             </div>
+//             <div className="input_container">
+//               <input
+//                 type="password"
+//                 name="password"
+//                 placeholder=" "
+//                 className="input_field"
+//               />
+//               <label htmlFor="password" className="floating_label">Password</label>
+//               {/* Password validation code here */}
+//             </div>
+//             <div className="login_link">
+//               Already have an account? &nbsp;&nbsp;<a href="http://localhost:5173/login">Login </a>
+//             </div>
+//             <button type="submit" className="submit_button">Submit</button>
+            
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Signup;
